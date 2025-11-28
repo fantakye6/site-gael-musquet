46bd003c2bc2fcd5b6186416b3a742793f5edac8
fantakye6 <fanta.kouyate635@gmail.com>
2025-11-28 13:37:51 +0100

    maj

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const content = `Météorologue de formation et hacker citoyen,
 Gaël Musquet place la technologie au service de l’humain.
 
 À travers des initiatives comme HAND et OpenStreetMap,
-il mobilise la cartographie et les données libres
+il mobilise la cartographie et les données ouvertes
 pour sauver des vies et redonner du pouvoir aux citoyens.
 
 Son engagement illustre une conviction simple :
@@ -19,7 +19,7 @@ function typeEffect() {
     if (index < content.length) {
         hackerText.textContent += content.charAt(index);
         index++;
-        setTimeout(typeEffect, 25); // vitesse d’écriture
+        setTimeout(typeEffect, 15); // vitesse d’écriture
     }
 }
 
