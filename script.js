720a17e00807426ec22955c1b8eb002bacc87d35
fantakye6 <fanta.kouyate635@gmail.com>
2025-11-28 16:34:41 +0100

    maj

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-
 // ===== Animation texte effet hacker =====
-const hackerText =  `Météorologue de formation et hacker citoyen,
+const hackerText = `Météorologue de formation et hacker citoyen,
 Gaël Musquet place la technologie au service de l’humain.
 
 À travers des initiatives comme HAND et OpenStreetMap,
@@ -12,32 +11,27 @@ la technologie n’a de sens que si elle renforce
 la solidarité et la résilience collective.`;
 
 let i = 0;
-const toggle = document.getElementById("nav-toggle");
-const menu = document.getElementById("nav-menu");
-
-toggle.addEventListener("click", () => {
-    menu.classList.toggle("show");
-});
 const hackerElement = document.getElementById("hacker-text");
-function typeWritter() {
-    if ( i < hackerText.length ) {
+
+function typeWriter() {
+    if (i < hackerText.length) {
         hackerElement.innerHTML += hackerText.charAt(i);
         i++;
-        setTimeout(typeWritter, 35); // vitesse d’écriture
+        setTimeout(typeWriter, 35); // vitesse d’écriture
     }
-const navToggle = document.getElementById('nav-toggle');
-    const navMenu = document.getElementById('nav-menu');
-    
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.toggle('show');
-    }); 
-    
 }
-typeWritter();
 
-/* ---------------------------------------
-   2) Animation des images de la GALERIE
------------------------------------------*/
+typeWriter();
+
+// ===== Menu responsive =====
+const navToggle = document.getElementById('nav-toggle');
+const navMenu = document.getElementById('nav-menu');
+
+navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+});
+
+// ===== Animation des images de la galerie =====
 const galleryImages = document.querySelectorAll('.gallery img');
 
 window.addEventListener('scroll', () => {
